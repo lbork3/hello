90b456c265794fafe30a292cd9c064fd3136ad79
lbork3 <111910849+lbork3@users.noreply.github.com>
2022-08-30 02:16:15 +0000

    inital commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Lexi Borkowski
+ *         lborkowski2@unl.edu
+ * Date: 2022/08/29
+ *
+ * Basic World Program
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello Lexi, Electircal Engineering\n");
+
+  return 0;
+}
